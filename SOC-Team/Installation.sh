8c2eb0dd70419a4bdec8013900e5b3882ed2d26b
ATUL9372 <>
2025-03-21 23:28:23 +0530

    Updated installation

--- SOC-Team/Installation.sh
+++ SOC-Team/Installation.sh
@@ -1,11 +1,12 @@
 cd /opt/
-sysctl -w vm.max_map_count=262144
-apt install git curl wget -y
+sudo sysctl -w vm.max_map_count=262144
+sudo apt install git curl wget -y
 
-git clone https://github.com/wazuh/wazuh-docker.git -b v4.10.1
+sudo git clone https://github.com/wazuh/wazuh-docker.git -b v4.10.1
 cd wazuh-docker/
-docker-compose -f generate-indexer-certs.yml run --rm generator
+cd single-node
+sudo docker-compose -f generate-indexer-certs.yml run --rm generator
 
 # Username : admin 
 # Password : SecretPassword
-docker-compose up -d
+sudo docker-compose up -d
